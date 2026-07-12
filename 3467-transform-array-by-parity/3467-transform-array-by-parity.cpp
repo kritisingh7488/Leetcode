02d9c6e6e1f0599d80953fb830bfdd0b1aaf4bb3
Kumari Kriti Singh <149036472+kritisingh7488@users.noreply.github.com>
2026-07-13 00:50:39 +0530

    Time: 0 ms (100%), Space: 28.3 MB (25.78%) - LeetHub

--- 3467-transform-array-by-parity/3467-transform-array-by-parity.cpp
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.cpp
@@ -2,15 +2,25 @@ class Solution {
 public:
     vector<int> transformArray(vector<int>& nums) {
         int n = nums.size();
+        int even=0;
+        int odd=0;
         for(int i =0; i<n;i++){
             if(nums[i]%2==0){
-                nums[i]=0;
+                // nums[i]=0;
+                even++;
             }
             else{
-                nums[i]=1;
+                // nums[i]=1;
+                odd++;
             }
         }
-        sort(nums.begin(),nums.end());
+        // sort(nums.begin(),nums.end());
+        for(int i =0; i<even;i++){
+            nums[i]=0;
+        }
+        for(int i =even;i<n;i++){
+            nums[i]=1;
+        }
         return nums;
     }
 };
