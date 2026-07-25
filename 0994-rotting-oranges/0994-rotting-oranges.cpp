4e36c9ee3acf00196aac5c572464624f16adb336
Kumari Kriti Singh <149036472+kritisingh7488@users.noreply.github.com>
2026-07-25 15:26:24 +0530

    Time: 0 ms (100%), Space: 16.6 MB (86.86%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -3,12 +3,8 @@ public:
     int dx[4] = {1,-1,0,0};
     int dy[4] = {0,0,1,-1};
 
-    void bfs(vector<vector<int>>& grid, queue<pair<int,int>>& q, int& fresh) {
-
-        int m = grid.size();
-        int n = grid[0].size();
-
-        int size = q.size();
+    void bfs(vector<vector<int>>& grid, queue<pair<int,int>>& q, int& fresh,int m,int n) {
+      int size = q.size();
 
         while(size--) {
 
@@ -23,7 +19,6 @@ public:
 
                 if(nx >= 0 && ny >= 0 && nx < m && ny < n &&
                    grid[nx][ny] == 1) {
-
                     grid[nx][ny] = 2;
                     fresh--;
                     q.push({nx, ny});
@@ -51,17 +46,17 @@ public:
             }
         }
 
-        int minutes = 0;
+        int t = 0;
 
         while(!q.empty() && fresh > 0) {
 
-            bfs(grid, q, fresh);
-            minutes++;
+            bfs(grid, q, fresh,m,n);
+            t++;
         }
 
         if(fresh > 0)
             return -1;
 
-        return minutes;
+        return t;
     }
 };
